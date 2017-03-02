fe37f2b66ae71a5d2dc763bcdf89981b94b3fa60
Tyler <tywilly300@gmail.com>
2017-03-02 17:55:35 -0500

    Setting up update thread

---
+++ src/meme/brownwillyengine/update/UpdateThread.java
@@ -0,0 +1,14 @@
+package meme.brownwillyengine.update;
+
+/**
+ * Created by Tyler on 3/2/2017.
+ */
+public class UpdateThread extends Thread implements Runnable{
+
+    @Override
+    public void run() {
+
+
+
+    }
+}
